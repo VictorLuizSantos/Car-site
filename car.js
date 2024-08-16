43615b7064b23eb3921a9ef79323cefd582247b5
VICTOR LUIZ MENDONÇA DOS SANTOS <2022001518@ifam.edu.br>
2024-08-15 22:49:51 -0400

    ilght/dark mode button added

--- car.js
+++ car.js
@@ -54,3 +54,15 @@ function zoomImg(posicao) {
     
     new bootstrap.Modal('#zoomImg').show()
 }
+
+ function alterarTemaSite() {
+    let tema = document.querySelector("html").getAttribute("data-bs-theme")
+    if(tema === "dark") {
+        document.querySelector("html").setAttribute("data-bs-theme", "light")
+        document.querySelector("#alterarTemaSite").innerHTML = `<i class="bi bi-moon-fill"></i>`
+    }
+    else {
+        document.querySelector("html").setAttribute("data-bs-theme", "dark")
+        document.querySelector("#alterarTemaSite").innerHTML = `<i class="bi bi-brightness-high-fill"></i>`
+    }
+ }
